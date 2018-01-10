f05125dd3a698be14028a946664a9f1682b8914e
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-01-10 17:32:30 +0000

    initial commit

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,25 @@
 const express = require('express')
 const router = express.Router()
+const request = require('sync-request');
 
 // Route index page
 router.get('/', function (req, res) {
   res.render('index')
+})
+
+router.post('/search', function (req, res) {
+  console.log(req.body)
+  debugger
+  const api_request = request('GET', 'http://localhost:9022/v2/uk/addresses', {
+  'headers': {
+    'X-LOCALHOST-Origin': 'test'
+  },
+  'qs': req.body
+});
+
+const api_response = JSON.parse(api_request.getBody('utf-8'))
+
+  res.render('search', {api_response: api_response} )
 })
 
 // Add your routes here - above the module.exports line
